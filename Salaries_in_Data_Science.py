42dcfc0a4d0cffd16acbc0be3c97136a95acab9e
Rosh C <43540251+RCChan5@users.noreply.github.com>
2022-12-05 00:01:15 -0500

    Update Salaries_in_Data_Science.py

--- Salaries_in_Data_Science.py
+++ Salaries_in_Data_Science.py
@@ -91,24 +91,6 @@ st.write("#### Job Title: "+str(df2.max()["Job Title"]) )
 st.write("#### Company: " + df2.max()["company_txt"] )
 st.write("#### Salary: "+str(df2.max()["Avg Salary(K)"]) + " k")
 st.write("#### Education: " + df2.max()["Degree"] )
-st.write("try out maps and ya done")
-st.write("know your worth maybe skip")
-
-
-st.map(df2)
-def main():
-    html_temp = """<div class='tableauPlaceholder' 
-    id='viz1670124138937' style='position: relative'><noscript><a href='#'><img alt='HOW MUCH CAN YOU MAKE IN DATA SCIENCE?An analysis of jobs offered on Glassdoor ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Sa&#47;SalariesinDataScience_16694182554330&#47;Story1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  
-    style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='SalariesinDataScience_16694182554330&#47;Story1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' 
-    /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Sa&#47;SalariesinDataScience_16694182554330&#47;Story1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' />
-    <param name='display_count' value='yes' /><param name='language' value='en-US' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1670124138937');                 
-       var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');              
-             scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
-    components.html(html_temp)
-if __name__ == "__main__":    
-    main()
-
-
 
 
 
